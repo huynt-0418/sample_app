ce36565b516f5314e660d7dbcd408029c90b1515
huynt-0418 <nguyen.tuan.huy@sun-asterisk.com>
2024-10-17 12:55:01 +0700

    Chapter 3,4,5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+  
+    get "static_pages/home"
+    get "static_pages/help"
+    get "static_pages/contact"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
